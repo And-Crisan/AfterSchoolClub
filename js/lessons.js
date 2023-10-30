9853f5de7a710bf2f372aa5e7c440c12ef73bb99
And-Crisan <147638606+And-Crisan@users.noreply.github.com>
2023-10-30 21:54:34 +0000

    put the items in a different js file

---
+++ js/lessons.js
@@ -0,0 +1,95 @@
+var lessonsData = [
+    // ... All your product objects go here ...
+
+    {
+        id: 1,
+        subject: 'Maths',
+        location: 'Harrow',
+        description: 'Maths is fun',
+        price: 0,
+        availableInventory: 5,
+        image: '../images/maths.png'
+    },
+    {
+        id: 2,
+        subject: 'English',
+        location: 'Watford',
+        description: 'Learn English with us',
+        price: 12,
+        availableInventory: 5,
+        image: '../images/english.jpg'
+    },
+    {
+        id: 3,
+        subject: 'Science',
+        location: 'Ealing',
+        description: 'Discover the wonders of science',
+        price: 15,
+        availableInventory: 5,
+        image: '../images/science.jpg'
+    },
+    {
+        id: 4,
+        subject: 'History',
+        location: 'Bushey',
+        description: 'Explore the past with us',
+        price: 8,
+        availableInventory: 5,
+        image: '../images/history.jpg'
+    },
+    {
+        id: 5,
+        subject: 'Media Studies',
+        location: 'Leavesden',
+        description: 'Unleash your creativity',
+        price: 20,
+        availableInventory: 5,
+        image: '../images/media.jpg'
+    },
+    {
+        id: 6,
+        subject: 'Music',
+        location: 'Burnt Oak',
+        description: 'Learn to play an instrument',
+        price: 18,
+        availableInventory: 5,
+        image: '../images/music.jpg'
+    },
+    {
+        id: 7,
+        subject: 'Enomomics',
+        location: 'Pinner',
+        description: 'Build your own future',
+        price: 25,
+        availableInventory: 8,
+        image: '../images/econ.jpg'
+    },
+    {
+        id: 8,
+        subject: 'Religious Studies',
+        location: 'South Harrow',
+        description: 'Know God',
+        price: 30,
+        availableInventory: 5,
+        image: '../images/rs.jpg'
+    },
+    {
+        id: 9,
+        subject: 'Drama',
+        location: 'Harrow on the Hill',
+        description: 'Acting and improvisation',
+        price: 22,
+        availableInventory: 5,
+        image: '../images/drama.webp'
+    },
+    {
+        id: 10,
+        subject: 'Sports',
+        location: 'Wembley',
+        description: 'Get fit and have fun',
+        price: 15,
+        availableInventory: 5,
+        image: '../images/pe.jpg'
+    }
+]
+
